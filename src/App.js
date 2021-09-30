ec364aa225df43d54596f7da1be2727bcbda98cb
Maiav27 <44206589+Maiav27@users.noreply.github.com>
2021-09-30 14:35:27 -0300

    add meses

--- src/App.js
+++ src/App.js
@@ -1,48 +1,46 @@
 
 //import { useEffect, useReducer } from 'react';
-import init from './Hooks'
+import Header from './elements/Header';
+import Meses from './Meses'
 
 
-const baseURL = 'https://mymoney-maia-default-rtdb.firebaseio.com/'
 
-
-const {useGet, usePost, useDelete} = init(baseURL)
 
 function App() {
-  const data = useGet('movimentcoes/2019-08')
+ // const data = useGet('movimentcoes/2019-08')
 
-  const [postData, post] = usePost('movimentcoes/2019-08')
-  const [deleteData, remove] = useDelete()
+ 
 
-  const saveNew = () => {
-    post({valor : 10, descricao : 'olaá'})
-  }
+ // const [postData, post] = usePost('movimentcoes/2019-08')
+ // const [deleteData, remove] = useDelete()
 
-  const doRemove = () =>{
-    remove('movimentcoes/2019-08/-MkO4-MW5GXL2GjKp4NS')
-  }
-  
+ // const saveNew = () => {
+ ///   post({valor : 10, descricao : 'olaá'})
+ // }
+
+ // const doRemove = () =>{
+//remove('movimentcoes/2019-08/-MkO4-MW5GXL2GjKp4NS')
+ //s }
+
   return (
     <div >
-      <nav className="navbar navbar-light bg-light">
-         <div className="container">
-            <a  className="navbar-brand">MyMoney</a>
-         </div>
-      </nav>
-    <select name="" id="">
-        <option value="2019">2019</option>
-        <option value="2020">2020</option>
-    </select>
-    <select name="" id="">
-        <option value="">01</option>
-        <option value="">02</option>
-    </select>
-      {JSON.stringify(data)}
-      {JSON.stringify(postData)}
-      {data.loading && <p>loading...</p>}
-     <button onClick={saveNew}>Salvar</button>
-     <button onClick={doRemove}>Remover</button>
-     {JSON.stringify(deleteData)}
+     <Header/>
+    <div className="container">
+          <select name="" id="">
+              <option value="2019">2019</option>
+              <option value="2020">2020</option>
+          </select>
+          <select name="" id="">
+              <option value="01">01</option>
+              <option value="02">02</option>
+          </select>
+          <button>Adicionar mês</button>
+          <Meses/>
+          
+           
+    </div>
+    
+
     </div>
   );
 }
---
+++ src/Meses.js
@@ -0,0 +1,53 @@
+import init from './Hooks'
+
+
+const baseURL = 'https://mymoney-maia-default-rtdb.firebaseio.com/'
+
+
+const {useGet, usePost, useDelete} = init(baseURL)
+
+const Meses = () =>{
+    const data = useGet('meses')
+    if(data.loading){
+        return <span>Carregando...</span>
+    }
+
+    if(data.data){
+
+    
+    return(
+    
+                <table className='table'>
+                    <thead>
+                        <tr>
+                        <th>Mês</th>
+                        <th>Previsão Entrada</th>
+                        <th>Entrada</th>
+                        <th>Previsão Saída</th>
+                        <th>Saída</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                    {
+                        Object.keys(data.data).map(mes =>{
+                        console.log(mes)
+                        return(
+                            <tr key={mes}>
+                                <td>{mes}</td>
+                                <td>{data.data[mes].previsao_entrada}</td>
+                                <td>{data.data[mes].entradas}</td>
+                                <td>{data.data[mes].previsao_saida}</td>
+                                <td>{data.data[mes].saidas}</td>
+                                </tr>
+                        )
+                        })
+                    }
+                    
+                    </tbody>
+                </table>
+                )
+    }
+     
+}
+
+export default Meses
