855a8f61bf223473123ef3ab2a17aa7af55942f3
Maiav27 <44206589+Maiav27@users.noreply.github.com>
2021-09-30 10:54:01 -0300

    ultimas atualizações

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+
+//import { useEffect, useReducer } from 'react';
+import init from './Hooks'
+
+
+const baseURL = 'https://mymoney-maia-default-rtdb.firebaseio.com/'
+
+
+const {useGet, usePost, useDelete} = init(baseURL)
 
 function App() {
+  const data = useGet('movimentcoes/2019-08')
+
+  const [postData, post] = usePost('movimentcoes/2019-08')
+  const [deleteData, remove] = useDelete()
+
+  const saveNew = () => {
+    post({valor : 10, descricao : 'olaá'})
+  }
+
+  const doRemove = () =>{
+    remove('movimentcoes/2019-08/-MkO4-MW5GXL2GjKp4NS')
+  }
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div >
+      <nav className="navbar navbar-light bg-light">
+         <div className="container">
+            <a  className="navbar-brand">MyMoney</a>
+         </div>
+      </nav>
+    <select name="" id="">
+        <option value="2019">2019</option>
+        <option value="2020">2020</option>
+    </select>
+    <select name="" id="">
+        <option value="">01</option>
+        <option value="">02</option>
+    </select>
+      {JSON.stringify(data)}
+      {JSON.stringify(postData)}
+      {data.loading && <p>loading...</p>}
+     <button onClick={saveNew}>Salvar</button>
+     <button onClick={doRemove}>Remover</button>
+     {JSON.stringify(deleteData)}
     </div>
   );
 }
---
+++ src/Hooks/index.js
@@ -0,0 +1,86 @@
+
+import axios from "axios";
+import { useReducer, useEffect} from "react";
+
+
+const reducer = (state, action) =>{
+    console.log('state', state, 'action', action)
+    if(action.type === 'REQUEST'){
+      return {
+        ...state,
+        loading : true
+      }
+    }
+     if(action.type === 'SUCCESS'){
+       return {
+         ...state, 
+         loading : false, 
+         data : action.data 
+       }
+     }
+     
+     
+  
+    return state
+  }
+
+
+  const init =  baseURL => {
+    const useGet = resource => {
+        const [data, dispatch] = useReducer(reducer, {
+          loading : true,
+          data : {}
+        } )
+          
+          useEffect(()=>{
+           dispatch({type : 'REQUEST'})
+             axios.get(baseURL + resource + '.json')
+             .then(res => {
+               dispatch({type : 'SUCCESS', data : res.data})
+               })
+             
+          },[resource])
+        return data
+      } 
+
+      const usePost = (resource) =>{
+        const [data, dispatch] = useReducer(reducer, {
+            loading : true,
+            data : {}
+          } )
+            
+        const post = (data) => {
+            dispatch({type : 'REQUEST'})
+            axios.post(baseURL + resource + '.json' , data)
+            .then(res => {
+                dispatch({type : 'SUCCESS',
+                      data : res.data})
+                console.log(res.data)})
+          }
+          return [data, post]
+    }
+
+    const useDelete = () =>{
+        const [data, dispatch] = useReducer(reducer, {
+            loading : true,
+            data : {}
+          } )
+            
+        const remove = (resource) => {
+            dispatch({type : 'REQUEST'})
+            axios.delete(baseURL + resource + '.json')
+            .then(() => {
+                dispatch({type : 'SUCCESS',
+                      })
+                })
+          }
+          return [data, remove]
+    }
+
+    return{ useGet, usePost, useDelete}
+  }
+
+  export default init
+
+
+
