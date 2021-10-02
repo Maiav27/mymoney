3f9e7abbc78db9c01cf56398452f26b42b3024e9
Maiav27 <44206589+Maiav27@users.noreply.github.com>
2021-10-02 08:44:24 -0300

    adicionando home

---
+++ src/Home.js
@@ -0,0 +1,14 @@
+import AdicionarMes from "./AdicionarMes"
+import Meses from "./Meses"
+
+const Home = () =>{
+    return(
+        <div className="container">        
+        <AdicionarMes/>
+        <Meses/>       
+        </div>
+    )
+}
+
+
+export default Home
