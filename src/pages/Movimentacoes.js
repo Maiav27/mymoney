79fa52a570d20c5423d77c0b3968782818e3793c
Maiav27 <44206589+Maiav27@users.noreply.github.com>
2021-10-06 08:22:40 -0300

    salvando movimentações

--- src/pages/Movimentacoes.js
+++ src/pages/Movimentacoes.js
@@ -1,9 +1,68 @@
+import init from '../Hooks'
+import { useState } from 'react'
+
+const baseURL = 'https://mymoney-maia-default-rtdb.firebaseio.com/movimentcoes/'
+
+const {useGet, usePost} = init(baseURL)
 
 
-const Movimentacoes= () => {
+
+
+const Movimentacoes= ({match}) => {
+    const data = useGet(match.params.data)
+    const [descricao, setDescricao] = useState('')
+    const [valor, setValor] = useState(0)
+
+    const  [postData, post] = usePost(match.params.data)
+
+
+    const onChange = tipo => evt =>{
+        tipo === 'descricao' ? setDescricao(evt.target.value) : setValor(evt.target.value)
+    }
+
+    const salvarMovimentacao = () =>{
+        post({
+             descricao : descricao , 
+            valor : valor
+        })
+
+        console.log(descricao, valor)
+    }
+        
+    
     return(
-        <div>
+        <div className='container'>
            <h1>Movimentações</h1>
+           <table className='table'>
+              <thead>
+                <tr>
+                <th>Descrição</th>
+                <th>Valor</th>
+                </tr>
+              </thead>
+              <tbody>
+                {
+                    Object.keys(data.data).map(movimentacao =>{
+                        return(
+                            <tr>
+                              <td>{data.data[movimentacao].descricao}</td>
+                              <td>{data.data[movimentacao].valor} </td>
+                            </tr>
+                        )
+                    }
+                        )
+                }
+                <tr>
+                    <td><input type="text" value={descricao} onChange={onChange('descricao')} /></td>
+                    <td>
+                        <input type="text" value={valor} onChange={onChange('valor')} />
+                        <button onClick={salvarMovimentacao}>+</button>
+                    </td>
+                </tr>
+              </tbody>
+              
+           </table>
+           {JSON.stringify(data.data)}
         </div>
     )
 }
