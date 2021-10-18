ead46ad56d1fd7aa8f729f0e7ab3f36231085265
Maiav27 <44206589+Maiav27@users.noreply.github.com>
2021-10-18 07:54:23 -0300

    Utilizando useRef

--- src/pages/Home/AdicionarMes.js
+++ src/pages/Home/AdicionarMes.js
@@ -1,17 +1,47 @@
 
+import { useState, useRef } from "react"
+import { Redirect } from "react-router"
+const minAno = 2019
+const maxAno = 2022
 
 const AdicionarMes = () =>{
+     const refAno = useRef()
+     const refMes = useRef()
+     const [redir, setRedir] = useState('')
+    const anos = []
+    const meses = []
+     for(let i = minAno; i <= maxAno ; i ++){
+         anos.push(i)
+     }
+     for(let i = 1; i <= 12; i++){
+         if(i < 10){
+
+             meses.push('0' + i)
+         }else{
+             meses.push(i)
+         }
+     }
+
+
+     const verMes = () =>{
+         setRedir(`${refAno.current.value}-${refMes.current.value} `  )
+
+     }
+     if(redir !== ''){
+         return <Redirect to={`/movimentacoes/${redir}`} />
+     }
+   
     return(
         <>
-            <select name="" id="">
-                <option value="2019">2019</option>
-                <option value="2020">2020</option>
+            <select name="" id="" style={{marginRight : '10px'}} ref={refAno}>
+                {anos.map(ano => <option key={ano}  value={ano}>{ano}</option>)}
+                
+                
             </select>
-            <select name="" id="">
-                <option value="01">01</option>
-                <option value="02">02</option>
+            <select  id="" style={{marginRight : '10px'}}  ref={refMes}>
+                {meses.map(mes => <option key={mes} value={mes}>{mes}</option>)}
             </select>
-            <button>Adicionar mês</button>
+            <button className='btn btn-success' onClick={verMes}>Adicionar mês</button>
         </>
 
     )
